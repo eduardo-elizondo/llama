d86b33724978d643b2f15975a437d2698a73b039
Joseph Milla <jjtmilla@gmail.com>
2015-04-10 23:49:04 -0500

    Setup framework

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('llamaApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
